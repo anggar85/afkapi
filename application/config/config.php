3dbef6bc209dd2f96de903a7104a925769c14304
Angel Canales <angel.canales@docdigitales.com>
2019-07-26 11:21:49 -0700

    Se configuira base de datos y url base

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.mxl-apps.com/goldenacorncasino/';
+// $config['base_url'] = 'https://www.mxl-apps.com/afkapi/';
+$config['base_url'] = 'http://localhost/afkapi/';
 
 /*
 |--------------------------------------------------------------------------
