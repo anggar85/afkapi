ef71e1e24585862b1e5c09dcae9e5727dee28e2f
Angel Canales <anggar85@gmail.com>
2019-11-08 01:28:43 -0800

    Se agrega js para cambiar el tamano de las imagenes

--- application/views/dashboard/news/show.php
+++ application/views/dashboard/news/show.php
@@ -1,5 +1,13 @@
-<p>
-	ojefoejf
-</p>
-
 <?php echo $data['data']['news']['desc']; ?>
+
+
+<script>
+	var images = document.getElementsByTagName('img')
+	console.log(images.length)
+	for (x = 0; x < images.length; x++){
+        images[x].removeAttribute('sizes')
+        images[x].removeAttribute('srcset')
+	    images[x].minWidth = screen.availWidth
+	}
+
+</script>
