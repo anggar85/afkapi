fd0d56445c584148f1cf2e1f6b0e2c10d26492df
Angel <anggar85@gmail.com>
2019-11-14 14:53:26 -0800

    Se limpia controller
    Se limpia modelo
    Se obtiene la foto del usuario desde el query usando la imagen guardada en el server

--- application/models/api/v2/Deck_model.php
+++ application/models/api/v2/Deck_model.php
@@ -8,45 +8,21 @@ class Deck_model extends CI_Model {
 
 	}
 
-    public function mydecks($user_id){
-        try{
-            // Busca los decks del usuario
-            $query = "SELECT  *  FROM decks  where  `user_id` = ".$user_id." order by `id` desc";
-
-            $q = $this->db->query($query);
-            $decks = [];
-            foreach($q->result() as $deck){
-                $deck = (array) $deck;
-                $deck['hero1'] = getImage($deck['hero1']); 
-                $deck['hero2'] = getImage($deck['hero2']); 
-                $deck['hero3'] = getImage($deck['hero3']); 
-                $deck['hero4'] = getImage($deck['hero4']); 
-                $deck['hero5'] = getImage($deck['hero5']);
-                array_push($decks, $deck);
-            }
-            
-            $response['error']  = false;
-            $response['data']['decks']    = $decks;
-            
-            return ($response);
-        }catch (Exception $e){
-
-            $response['error']  = true;
-            $response['msg']   = $e->getMessage();
-            return ($response);
-        }
-    }
-
     public function decks_list($columna, $asc_desc){
         try{
-            $query = "SELECT  d.*, u.id as userId, u.token as user_token, 
-            (select count('id') from votes where item_id = d.id) as numero_votos  FROM decks as d 
+
+            $baseUrl = base_url('assets/images/users/user_');
+
+            $query = 'SELECT  d.*, u.id as userId, u.token as user_token, 
+            (select count("id") from votes where item_id = d.id) as numero_votos,  
+            concat("'.$baseUrl.'", u.id) as fb_image
+            FROM decks as d 
             JOIN users as u
             ON u.id = d.user_id
-            where d.status = 1 
-            AND d.name != '' AND d.desc != ''
-            AND d.hero1 != '' AND d.hero2 != '' AND d.hero3 != '' AND d.hero4 != '' AND d.hero5 != ''
-             order by $columna $asc_desc";
+            where d.status = "1" 
+            AND d.name != "" AND d.desc != ""
+            AND d.hero1 != "" AND d.hero2 != "" AND d.hero3 != "" AND d.hero4 != "" AND d.hero5 != ""
+            order by '.$columna.' '.$asc_desc;
 
             $q = $this->db->query($query);
             $decks = [];
@@ -57,7 +33,7 @@ class Deck_model extends CI_Model {
                 $deck['hero3'] = getImage($deck['hero3']); 
                 $deck['hero4'] = getImage($deck['hero4']); 
                 $deck['hero5'] = getImage($deck['hero5']);
-                $deck['fb_image'] = getProfilePic($deck['user_token']);
+                // $deck['fb_image'] = getProfilePic($deck['user_token']);
                 array_push($decks, $deck);
             }
             
